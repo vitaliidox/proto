08672c7039ae49aff5a23d375642b9c2fe96cbbd
vitaliidox <72409488+vitaliidox@users.noreply.github.com>
2021-04-27 20:05:19 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+//Task 1
+// function calc(sum, percent, years){
+//     let deposit = sum;
+//     for(let i = 0; i<years;i++){
+//         deposit +=sum*(percent/100)
+//     }
+//     console.log(deposit)
+// }
+// calc(10000, 5, 3)
+
+//Task 2
+
+// let head = {
+//     glasses: 1
+// };
+
+// let table = {
+//     pen: 3
+// };
+
+// let bed = {
+//     sheet: 1,
+//     pillow: 2
+// };
+
+// let pockets = {
+//     money: 2000
+// };
+
+// pockets.__proto__ = bed
+// bed.__proto__ = table
+// table.__proto__ = head
+
+// console.log(pockets.sheet)
